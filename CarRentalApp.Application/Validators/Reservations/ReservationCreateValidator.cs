bb98aac9b7316777a943b97a13f7f159a18158a9
mahamdeh0 <mahamdehabd@gmail.com>
2024-11-18 14:06:19 +0200

    Add FluentValidation for Reservation creation and update DTOs

---
+++ CarRentalApp.Application/Validators/Reservations/ReservationCreateValidator.cs
@@ -0,0 +1,16 @@
+﻿using CarRentalApp.Application.Models.Reservation;
+using FluentValidation;
+
+namespace CarRentalApp.Application.Validators.Reservations
+{
+    public class ReservationCreateValidator : AbstractValidator<ReservationCreateDto>
+    {
+        public ReservationCreateValidator()
+        {
+            RuleFor(x => x.CarId).GreaterThan(0).WithMessage("CarId is required");
+            RuleFor(x => x.UserId).GreaterThan(0).WithMessage("UserId is required");
+            RuleFor(x => x.StartDate).LessThan(x => x.EndDate).WithMessage("StartDate must be before EndDate");
+            RuleFor(x => x.TotalCost).GreaterThan(0).WithMessage("Total cost must be greater than 0");
+        }
+    }
+}
