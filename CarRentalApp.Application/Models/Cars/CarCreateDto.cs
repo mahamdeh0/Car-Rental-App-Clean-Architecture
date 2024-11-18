dbbe3ea13598115423a627b0dd41699637d0ea12
mahamdeh0 <mahamdehabd@gmail.com>
2024-11-18 13:46:43 +0200

    Add CarDto, CarCreateDto, and CarUpdateDto models for car data transfer

---
+++ CarRentalApp.Application/Models/Cars/CarCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRentalApp.Application.Models.Cars
+{
+    public record CarCreateDto
+    (
+        string Brand,
+        string Model,
+        decimal PricePerDay,
+        bool IsAvailable
+    );
+}
