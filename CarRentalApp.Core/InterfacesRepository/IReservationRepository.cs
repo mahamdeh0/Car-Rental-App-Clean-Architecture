3b3484d374785e19c1bd25ed03384f90e05fcec0
mahamdeh0 <mahamdehabd@gmail.com>
2024-11-18 12:45:32 +0200

    Add IReservationRepository interface for reservation-related data operations

---
+++ CarRentalApp.Core/InterfacesRepository/IReservationRepository.cs
@@ -0,0 +1,13 @@
+﻿using CarRentalApp.Core.Entities;
+
+namespace CarRentalApp.Core.InterfacesRepository
+{
+    public interface IReservationRepository
+    {
+        Task<List<Reservation>> GetReservationsByUserIdAsync(int userId);
+        Task<Reservation> GetReservationByIdAsync(int reservationId);
+        Task AddReservationAsync(Reservation reservation);
+        Task UpdateReservationAsync(Reservation reservation);
+        Task DeleteReservationAsync(int reservationId);
+    }
+}
