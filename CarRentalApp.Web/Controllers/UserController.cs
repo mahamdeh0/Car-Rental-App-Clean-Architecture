a5d0659eea287c0d710dc1ebfc328066a5053a29
mahamdeh0 <mahamdehabd@gmail.com>
2024-11-18 16:45:22 +0200

    Add UserController for managing user information (CRUD operations) in CarRentalApp.Web.Controllers

---
+++ CarRentalApp.Web/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using CarRentalApp.Application.InterfacesService;
+using CarRentalApp.Application.Models.Users;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var users = await _userService.GetAllUsersAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(int id)
+    {
+        var user = await _userService.GetUserByIdAsync(id);
+        return Ok(user);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, [FromBody] UserUpdateDto userUpdateDto)
+    {
+        await _userService.UpdateUserAsync(id, userUpdateDto);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        await _userService.DeleteUserAsync(id);
+        return NoContent();
+    }
+}
