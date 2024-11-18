10c07396166956c4001b8e0c441da7a4f5940448
mahamdeh0 <mahamdehabd@gmail.com>
2024-11-18 16:38:30 +0200

    Add UserLoginDto class for handling user login details

---
+++ CarRentalApp.Application/Models/Users/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CarRentalApp.Application.Models.Users
+{
+    public class UserLoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
