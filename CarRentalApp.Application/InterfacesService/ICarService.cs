231228b28862ff77b76138a4bfa9d70ecb61142e
mahamdeh0 <mahamdehabd@gmail.com>
2024-11-18 15:07:07 +0200

    Add ICarService interface for car management functionality

---
+++ CarRentalApp.Application/InterfacesService/ICarService.cs
@@ -0,0 +1,13 @@
+﻿using CarRentalApp.Application.Models.Cars;
+
+namespace CarRentalApp.Application.InterfacesService
+{
+    public interface ICarService
+    {
+        Task<List<CarDto>> GetAvailableCarsAsync();
+        Task<CarDto> GetCarByIdAsync(int id);
+        Task AddCarAsync(CarCreateDto carCreateDto);
+        Task UpdateCarAsync(int id, CarUpdateDto carUpdateDto);
+        Task DeleteCarAsync(int id);
+    }
+}
