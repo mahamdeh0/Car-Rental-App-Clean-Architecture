c100f2afe2b584192b8f78956c19e9093d034e18
mahamdeh0 <mahamdehabd@gmail.com>
2024-11-18 15:22:34 +0200

    Configure dependency injection and services in Program.cs

--- CarRentalApp.Web/Program.cs
+++ CarRentalApp.Web/Program.cs
@@ -1,16 +1,33 @@
+﻿using CarRentalApp.Application.InterfacesService;
+using CarRentalApp.Application.Services;
+using CarRentalApp.Application.Validators.Cars;
+using CarRentalApp.Core.InterfacesRepository;
 using CarRentalApp.Infrastructure.Data;
+using CarRentalApp.Infrastructure.Repositories;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CarCreateValidator>());
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IReservationService, ReservationService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICarService, CarService>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
